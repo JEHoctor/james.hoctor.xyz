dcee6e6939ac50c5c1a9a27c85a03815ed32be94
James Hoctor <JEHoctor@protonmail.com>
2025-05-23 00:50:39 -0400

    Add bio and profile picture

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,6 +10,14 @@ DEFAULT_LANG = "en"
 
 THEME = "hyde-personalized"
 
+# Settings specific to the theme
+BIO = """
+I am a machine learning engineer and a data scientist.
+My interests include natural language processing, computer vision, and interpretable machine learning.
+I hold an MS in Computer Science from Duke University, and a BS in Mathematics from Rensselaer Polytechnic Institute.
+"""
+PROFILE_IMAGE = "github-profile.jpg"
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
