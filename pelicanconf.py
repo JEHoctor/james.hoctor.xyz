c34b34da91e7e6d6f2cc5586c220736e7bed40c1
James Hoctor <JEHoctor@protonmail.com>
2025-05-23 21:50:53 -0400

    Prevent line breaks within e.g. BS Mathematics

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,7 @@ DEFAULT_LANG = "en"
 THEME = "hyde-personalized"
 
 # Settings specific to the theme
-BIO = "Machine learning engineer and data scientist. BS Mathematics, MS Computer Science."
+BIO = "Machine learning engineer and data scientist. <nobr>BS Mathematics</nobr>, <nobr>MS Computer</nobr> Science."
 PROFILE_IMAGE = "github-profile.jpg"
 COLOR_THEME = "08"
 
